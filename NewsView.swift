f539c08fa3789947b080b391b44331efe156100c
Kai Jing <kai@khanacademy.org>
2020-03-05 21:20:02 -0800

    to add a WebView to view web page of each google news in detail

--- NewsView.swift
+++ NewsView.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import WebKit
 import SwiftUI
 
 struct NewsView: View {
@@ -14,12 +15,30 @@ struct NewsView: View {
     let url: String?
 
     var body: some View {
-        Text("will add later")
+        WebView(urlString: url)
     }
 }
 
 struct NewsView_Previews: PreviewProvider {
     static var previews: some View {
         NewsView(url: "https://www.google.com")
+    }
+}
+
+struct WebView: UIViewRepresentable {
+
+    let urlString: String?
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
     }
 }
